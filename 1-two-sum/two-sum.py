f494b81f69030085faa1631113d90ca12f3c4fc4
Jashanpreet Singh <jashanpreet.sellopal@gmail.com>
2026-02-03 23:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (64.00%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,12 +1,13 @@
 class Solution(object):
     def twoSum(self, nums, target):
+
         n = len(nums)
-        rem = {}
+        freq = {}
         for i in range(0,n):
             need = target-nums[i]
-            if need in rem:
-                return [i,rem[need]]
-            rem[nums[i]]=i
+            if need in freq:
+                return [i,freq[need]]
+            freq[nums[i]]=i
                 
          
 
