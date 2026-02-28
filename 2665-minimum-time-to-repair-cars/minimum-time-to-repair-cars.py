c4be1d0d0e248dd4bbec850536802a6a0f2f1161
Jashanpreet Singh <jashanpreet.sellopal@gmail.com>
2026-02-28 15:13:10 +0530

    Time: 1097 ms (14.01%) | Memory: 23.8 MB (57.99%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -1,16 +1,16 @@
 class Solution:
     def repairCars(self, ranks, cars: int) -> int:
-        left = 1
-        right = min(ranks) * cars * cars   # maximum possible time      
-        while left < right:
-            mid = (left + right) // 2          
+        start = 1
+        end = min(ranks) * cars * cars        
+        while start < end:
+            mid = (start + end) // 2          
             total = 0
             for r in ranks:
                 total += int((mid // r) ** 0.5)          
             if total >= cars:
-                right = mid
+                end = mid
             else:
-                left = mid + 1       
-        return left
+                start= mid + 1       
+        return end
 
             
