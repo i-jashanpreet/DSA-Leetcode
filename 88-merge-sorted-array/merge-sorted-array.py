3c0ea473c65cfe4b737ab87f59c647af9ec5343a
Jashanpreet Singh <jashanpreet.sellopal@gmail.com>
2026-04-29 18:06:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (95.78%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,31 +1,27 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        f = []
+    def merge(self, nums1, m, nums2, n):
+        i = 0
         j = 0
-        k = 0
-        while j < m and k < n:
-            if nums1[j] < nums2[k]:
-                f.append(nums1[j])
-                j += 1
-            elif nums2[k] < nums1[j]:
-                f.append(nums2[k])
-                k += 1
+        ans = []
+
+        while i < m and j < n:
+            if nums1[i] <= nums2[j]:
+                ans.append(nums1[i])
+                i += 1
             else:
-                f.append(nums1[j])
+                ans.append(nums2[j])
                 j += 1
-                f.append(nums2[k])
-                k += 1
-        while j < m:
-            f.append(nums1[j])
+
+        while i < m:
+            ans.append(nums1[i])
+            i += 1
+
+        while j < n:
+            ans.append(nums2[j])
             j += 1
 
-        while k < n:
-            f.append(nums2[k])
-            k += 1
-        for i in range(m + n):
-            nums1[i] = f[i]
-        
+        nums1[:] = ans
+
+
+
         
