70cf1b6b8482a5fbf267a4600d5059dba32ec3c3
Jashanpreet Singh <jashanpreet.sellopal@gmail.com>
2026-05-04 14:04:44 +0530

    Time: 3 ms (8.06%) | Memory: 19.5 MB (66.66%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -6,15 +6,27 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        # def f(root,isleft):
+        #     if root==None:
+        #         return
+        #     if root.left==None and root.right==None and isleft==True:
+        #         lst.append(root.val)
+        #     f(root.left,True)
+        #     f(root.right,False)
+        #     return
+        # lst = []
+        # f(root,False)
+        # return sum(lst)
+
+
         def f(root,isleft):
             if root==None:
-                return
+                return 0
             if root.left==None and root.right==None and isleft==True:
-                lst.append(root.val)
-            f(root.left,True)
-            f(root.right,False)
-            return
-        lst = []
-        f(root,False)
-        return sum(lst)
+                return root.val
+            left = f(root.left,True)
+            right= f(root.right,False)
+            return left+right
+        return f(root,False)
+
         
