2a0e8d90e29fc88712a9e3b11890337d06d2791d
Jashanpreet Singh <jashanpreet.sellopal@gmail.com>
2026-07-09 11:11:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (31.64%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,23 +6,36 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        def f(node):
+        # def f(node):
+        #     if node==None:
+        #         return []
+        #     res =[]
+        #     q = [node]
+        #     while q:
+        #         n = len(q)
+        #         for i in range(n):
+        #             node = q.pop(0)
+        #             if i==n-1:
+        #                 res.append(node.val)
+        #             if node.left:
+        #                 q.append(node.left)
+        #             if node.right:
+        #                 q.append(node.right)
+        #     return res
+        # return f(root)
+        ans = []
+        def rev_post_order(node,level):
             if node==None:
-                return []
-            res =[]
-            q = [node]
-            while q:
-                n = len(q)
-                for i in range(n):
-                    node = q.pop(0)
-                    if i==n-1:
-                        res.append(node.val)
-                    if node.left:
-                        q.append(node.left)
-                    if node.right:
-                        q.append(node.right)
-            return res
-        return f(root)
+                return
+            if len(ans)==level:
+                ans.append(node.val)
+            if node.right:
+                rev_post_order(node.right,level+1)
+            if node.left:
+                rev_post_order(node.left,level+1)
+        rev_post_order(root,0)
+        return ans
+
 
 
 
